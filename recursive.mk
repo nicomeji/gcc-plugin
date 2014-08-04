c8ddf932749432852ffa47997a6fdf0af1b7d83f
Nicolas Mejias <nico.meji@gmail.com>
2014-08-04 16:10:37 -0300

    Adding test support.

--- recursive.mk
+++ recursive.mk
@@ -7,11 +7,6 @@ endif
 define RECURSIVE_RULES
 .PHONY: $1.%
 $1.%:
-ifdef SUBTARGET_DIR
-	$(eval export SUBTARGET_DIR=$(SUBTARGET_DIR))
-else
-	$(eval export SUBTARGET_DIR=$(strip $(TARGET_DIR))/$1)
-endif
-	$(MAKE) -C $1 -I "../$(PROJECT_ROOT)" -e PROJECT_ROOT="../$(.PROJECT_ROOT)" $$*
+	$(MAKE) -C $1 -I "../$(PROJECT_ROOT)" -e PROJECT_ROOT="../$(PROJECT_ROOT)" $(foreach var,$(EXPORT_VARS), -e $(var)) $$*
 endef
 $(foreach directory,$(RECURSIVE_SUBDIRS),$(eval $(call RECURSIVE_RULES,$(directory))))
