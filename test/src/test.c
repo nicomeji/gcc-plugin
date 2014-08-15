99bbb5ebf3eb27c13e8de0b359fb17a95896eddd
Nicolas Mejias <nico.meji@gmail.com>
2014-08-15 17:51:35 -0300

    Adding test support.

---
+++ test/src/test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "unity.h"
+
+char *output;
+
+TEST_SETUP(sprintf)
+{
+   output = malloc(100);
+}
+
+TEST_TEAR_DOWN(sprintf)
+{
+   free(output);
+}
+
+TEST (sprintf, NoFormatOperations)
+{
+   TEST_ASSERT_EQUAL(3,sprintf(output, "hey"));
+   TEST_ASSERT_EQUAL_STRING("hey", output);
+}
+
+TEST (sprintf, InsertString)
+{
+   TEST_ASSERT_EQUAL(3,sprintf(output, "Hello %s", "world"));
+   TEST_ASSERT_EQUAL_STRING("Hello world", output);
+}
--- test/src/test_runner.c
+++ test/src/test_runner.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "unity.h"
 #include "gcc-plugin.h"
+// #include "unity_fixture.h"
 
 diagnostic_context *global_dc = (diagnostic_context *) NULL;
 
@@ -16,6 +17,12 @@ void register_callback(const char *plugin_name, int event,
       plugin_callback_func callback, void *user_data) {
    printf("%s registered.", plugin_name);
 }
+
+//TEST_GROUP_RUNNER(sprintf)
+//{
+//   RUN_TEST_CASE(sprintf, NoFormatOperations);
+//   RUN_TEST_CASE(sprintf, InsertString);
+//}
 
 int main(void) {
    printf("Test run!!\n");
