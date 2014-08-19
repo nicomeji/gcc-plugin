d661fb8b5248abb0ee6459e9dee19b87e1df44c4
Nicolas Mejias <nico.meji@gmail.com>
2014-08-19 17:12:38 -0300

    Adding test support.

--- test/src/test.c
+++ test/src/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "unity.h"
+#include "unity_fixture.h"
 
 char *output;
 
--- test/src/test_runner.c
+++ test/src/test_runner.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
 #include "unity.h"
 #include "gcc-plugin.h"
-// #include "unity_fixture.h"
+#include "unity_fixture.h"
 
 diagnostic_context *global_dc = (diagnostic_context *) NULL;
-
-void setUp(void) {
-   printf ("Set up test.\n");
-}
-
-void tearDown(void) {
-   printf ("Tear down test.\n");
-}
 
 void register_callback(const char *plugin_name, int event,
       plugin_callback_func callback, void *user_data) {
    printf("%s registered.", plugin_name);
 }
 
-//TEST_GROUP_RUNNER(sprintf)
-//{
-//   RUN_TEST_CASE(sprintf, NoFormatOperations);
-//   RUN_TEST_CASE(sprintf, InsertString);
-//}
+TEST_GROUP_RUNNER(sprintf)
+{
+   RUN_TEST_CASE(sprintf, NoFormatOperations);
+   RUN_TEST_CASE(sprintf, InsertString);
+}
 
 int main(void) {
    printf("Test run!!\n");
